f467705aeabaf0f597f33466b01843010da39b8c
Mahashwe <maha.tomholland@gmail.com>
2026-02-06 11:30:54 +0530

    Class components

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/greet';
+import Welcome from './components/welcome';
 function App() {
   return (
     <div className="App">
       <Greet> </Greet>
+      <Welcome> </Welcome>
     </div>
   );
 }
